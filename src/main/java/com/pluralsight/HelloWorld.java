ecc41c760614fdfa4246a8d164f4a12d7f861a75
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-07 15:46:35 -0500

    Printing Paul Intro

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,7 +3,7 @@ package com.pluralsight;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Howdy, I'm Paul!");
     }
 
 
