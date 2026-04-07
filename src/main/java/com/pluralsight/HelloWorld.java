8689342bcb134d4698c1dcf0bd01c4bee2eedcf3
Joi McBrayer <j0ifulnois3@gmail.com>
2026-04-07 15:47:51 -0500

    Printing Outro

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -3,7 +3,8 @@ package com.pluralsight;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Howdy, I'm Paul!");
+        System.out.println("Hi, I'm Paul!");
+        System.out.println("Mom, Phineas and Ferb are taking coding classes!");
     }
 
 
